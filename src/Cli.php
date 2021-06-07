3a3d5a75114edaf6524e56eda67696d548b5042a
bbdb19 <bakdauren@gmail.com>
2021-06-07 14:41:30 +0600

    step 3

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Brain\Games;
+
+use function cli\line;
+use function cli\prompt;
+
+class Cli
+{
+  public function welcome()
+  {
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+  }
+}
